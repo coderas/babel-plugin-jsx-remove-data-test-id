b67502248c6d3f43a0470e7ded9752392437fc36
Kyle Gray <kgray1497@gmail.com>
2019-11-15 08:57:43 -0500

    chore: switched back to single quote

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 const getAttributeIdentifiers = options => {
-  if (!options || typeof options.attributes === "undefined")
-    return ["data-test-id", "data-testid"];
+  if (!options || typeof options.attributes === 'undefined')
+    return ['data-test-id', 'data-testid'];
 
   if (Array.isArray(options.attributes)) {
     if (options.attributes.length === 0) {
       throw new Error(
-        "option attributes must be an array with at least one element"
+        'option attributes must be an array with at least one element'
       );
     }
 
     if (
       options.attributes.length !==
-      options.attributes.filter(attr => attr && typeof attr === "string").length
+      options.attributes.filter(attr => attr && typeof attr === 'string').length
     ) {
       throw new Error(
-        "all items in the option attributes must be non empty strings"
+        'all items in the option attributes must be non empty strings'
       );
     }
 
     return options.attributes;
   }
 
-  if (!options.attributes || typeof options.attributes !== "string") {
+  if (!options.attributes || typeof options.attributes !== 'string') {
     throw new Error(
-      "option attributes must be a non empty string or an array with non empty strings"
+      'option attributes must be a non empty string or an array with non empty strings'
     );
   }
 
@@ -53,7 +53,7 @@ const RemoveDataTestIds = ({ types: t }) => {
           }
         };
 
-        const isDefined = value => typeof value !== "undefined";
+        const isDefined = value => typeof value !== 'undefined';
 
         const attrs = path.node.attributes.map(matchingAttrs).filter(isDefined);
 
